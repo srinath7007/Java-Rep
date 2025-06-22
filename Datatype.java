3ff3078b10f335f411409eea77724d4e35526845
srinath7007 <srinathsowmi7007@gmail.com>
2025-06-22 09:37:18 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,25 @@
+class Datatype
+{
+public static void main(String[] args)
+{
+byte z=12;
+short x=6;
+int c=123;
+long v=123456;
+
+float a=99.99f;
+double b=77.777777d;
+
+boolean s=true;
+char d='$';
+
+System.out.println(z);
+System.out.println(x);
+System.out.println(c);
+System.out.println(v);
+System.out.println(a);
+System.out.println(b);
+System.out.println(s);
+System.out.println(d);
+}
+}
---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+public static void main(String[] args)
+{
+	String name="Srinath";
+	
+System.out.println("welcome to the java session");
+System.out.println("full stack development");
+System.out.print("cricket\t\tfootball\n");
+System.out.print("name");
+}
+}
